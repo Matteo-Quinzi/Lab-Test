34a46813c31e3ee56ff64c580b20cdcd592136dd
alle.cuoghi <alle.cuoghi@live.com>
2021-05-25 12:50:35 +0200

    waves

--- Waves/task.f90
+++ Waves/task.f90
@@ -1,29 +1,21 @@
 MODULE TASK
         USE READ_VAR 
         CONTAINS
-                FUNCTION POT(x)
+                FUNCTION POT()
                         IMPLICIT NONE
-
-                        ! INPUT
-                        REAL(KIND=8), INTENT(IN) :: x(N)
-
+                        
                         ! OUTPUT 
                         REAL(KIND=8) :: POT(N)
 
                         ! ROUTINE
                         INTEGER(KIND=8) :: l1, l2
-
+                        
                         l1 = INT(N * l_1 / (2 * L))
                         l2 = INT(N * l_2 / (2 * L))
 
                         POT(:) = 0.D0
                         POT(  N/2 - l1 :   N/2 + l1) = E_1
                         POT(2*N/3 - l2 : 2*N/3 + l2) = E_2
-
-                        OPEN(UNIT = 10, FILE = 'Output/pot.txt')
-                          WRITE(10, '(2F20.10)') (x(i), POT(i), i = 1, N)
-                        CLOSE(10)
-
                 END FUNCTION POT
 
                 FUNCTION PHI(x) 
@@ -35,44 +27,28 @@ MODULE TASK
                         ! OUTPUT
                         COMPLEX(KIND=8) :: PHI(N)
 
-                        ! ROUTINE
-                        INTEGER :: IOS
-
                         IF (load_f_inp .eqv. .TRUE.) THEN
-                                OPEN(UNIT = 10, FILE = f_inp, STATUS = 'OLD', ACTION = 'READ', IOSTAT = IOS)
+                                OPEN(UNIT=10, FILE=f_inp, STATUS='OLD', ACTION='READ', IOSTAT=IOS)
                                 IF (IOS == 0) THEN
                                         READ(10, '(2F20.10)') (PHI(i), i = 1,N)
 
                                 ELSE
-                                        PRINT *, 'Error in opening file "', f_inp, '"'
+                                        PRINT *, 'Error in opening "', f_inp, '". Program stopping...'
                                         STOP
                                 ENDIF
 
                         ELSE
-                                PHI = PHI_GAUSS(x)
+                                PHI(:) = (2.D0 * pi * sigma**2)**(-0.25) * EXP(im * k0 * x(:)) * EXP(-(x(:) - x0)**2 / (4.D0 * sigma**2))
                         ENDIF
 
                 END FUNCTION PHI
 
-                FUNCTION PHI_GAUSS(x) RESULT(F)
-                        IMPLICIT NONE
-
-                        ! INPUT
-                        REAL(KIND=8), INTENT(IN) :: x(N)
-
-                        ! OUTPUT
-                        COMPLEX(KIND=8) :: F(N)
-                        
-                        F(:) = (2.D0 * pi * sigma**2)**(-0.25) * EXP(im * k0 * x(:)) * EXP(-(x(:) - x0)**2 / (4.D0 * sigma**2))
-
-                END FUNCTION PHI_GAUSS
-
-                FUNCTION FFT(F, DIR)
+                FUNCTION FFT(PHI, DIR)
                         IMPLICIT NONE
                         INCLUDE 'fftw3.f'
 
                         ! INPUT
-                        COMPLEX(KIND=8), INTENT(IN) :: F(N)
+                        COMPLEX(KIND=8), INTENT(IN) :: PHI(N)
                         CHARACTER(LEN=1), INTENT(IN) :: DIR
 
                         ! OUTPUT
@@ -82,14 +58,14 @@ MODULE TASK
                         INTEGER(KIND=8) :: plan
 
                         IF (DIR == 'F') THEN
-                                CALL DFFTW_PLAN_DFT_1D(plan, N, F, FFT, FFTW_FORWARD, FFTW_ESTIMATE)
+                                CALL DFFTW_PLAN_DFT_1D(plan, N, PHI, FFT, FFTW_FORWARD, FFTW_ESTIMATE)
 
                         ELSEIF (DIR == 'B') THEN
-                                CALL DFFTW_PLAN_DFT_1D(plan, N, F, FFT, FFTW_BACKWARD, FFTW_ESTIMATE)
+                                CALL DFFTW_PLAN_DFT_1D(plan, N, PHI, FFT, FFTW_BACKWARD, FFTW_ESTIMATE)
 
                         ENDIF
 
-                        CALL DFFTW_EXECUTE_DFT(plan, F, FFT)
+                        CALL DFFTW_EXECUTE_DFT(plan, PHI, FFT)
                         CALL DFFTW_DESTROY_PLAN(plan)
 
                 END FUNCTION FFT
--- Waves/waves.f90
+++ Waves/waves.f90
@@ -1,153 +1,124 @@
 PROGRAM MAIN
         USE READ_VAR
         USE TASK
-        USE OMP_LIB
+     
         IMPLICIT NONE
 
-        INTEGER :: l2, l1, cc=0, cd=0, i_ent, i_exi, j
-        REAL(KIND=8) :: peak, x_peak, ent, exi, t_ent, t_exi, time_hartman, T_B(15,4)
-
-        REAL(KIND=8), ALLOCATABLE :: x(:), V(:), k(:), F_EVOL(:,:), F_SQMOD(:)
-        COMPLEX(KIND=8), ALLOCATABLE :: V_OP(:), K_OP(:), F_X(:), F_K(:)
+        INTEGER :: l2, l1
+        REAL(KIND=8), ALLOCATABLE :: x(:), V(:), k(:), PHI_EVOL(:,:), PHI_SQMOD(:)
+        COMPLEX(KIND=8), ALLOCATABLE :: V_OP(:), K_OP(:), PHI_X(:), PHI_K(:)
+        
+        CALL TITLE()
 
         CALL READ_DATA()
         ALLOCATE(x(N), V(N), k(N))
         ALLOCATE(V_OP(N), K_OP(N))
-        ALLOCATE(F_X(N), F_K(N), F_SQMOD(N), F_EVOL(N, save_wave+1))
+        ALLOCATE(PHI_X(N), PHI_K(N), PHI_SQMOD(N), PHI_EVOL(N, save_wave+1))
         
         x(:) = (/ (i * L / (N - 1), i = 0, N-1) /)
         
         k( : N/2) = (/ (2.D0 * pi * i / L, i = 0, N/2-1) /)
         k(N/2 + 1 : ) = (/ (-2.D0 * pi / L * (N/2.D0 - i), i=0, N/2-1) /) 
-
-        DO j = 1,15
-        V = POT(x)
-
-        V_OP(:) = EXP(-im * V(:) * dt / 2.D0)
         K_OP(:) = EXP(-im * k(:)**2 * dt / 2.D0)
-
-        !$OMP PARALLEL PRIVATE(F_X, F_K) SHARED(F_EVOL)
-
-        F_X = PHI(x)
-
-        F_EVOL(:,1) = REAL(F_X(:))**2 + AIMAG(F_X(:))**2
-
-        PRINT *, 'INITIAL NORM:', SUM(F_EVOL(:,1)) * (x(2)-x(1))
+       
+        V = POT()
+        V_OP(:) = EXP(-im * V(:) * dt / 2.D0)
+        l1 = INT(N * l_1 / (2 * L))
+        l2 = INT(N * l_2 / (2 * L)) 
+ 
+        PHI_X = PHI(x)
+        PHI_EVOL(:,1) = REAL(PHI_X(:))**2 + AIMAG(PHI_X(:))**2
 
         save_timestep = M / save_wave
 
-        l1 = INT(N * l_1 / (2 * L))
-        l2 = INT(N * l_2 / (2 * L))
-
-        ent = 0.
-        exi = 0.
-
-        cc = 0
-        cd = 0
-        !$OMP DO
-        DO i = 1, M
-            F_X(:) = F_X(:) * EXP(-im * V(:) * dt/2.D0)
-            
-            F_K = FFT(F_X, 'F') 
-            F_K(:) = F_K(:) * EXP(-im * k(:)**2 * dt/2.D0) / N
-
-            F_X = FFT(F_K, 'B')
-            F_X(:) = F_X(:) * EXP(-im * V(:) * dt/2.D0)
-
-            F_SQMOD(:) = REAL(F_X(:))**2 + AIMAG(F_X(:))**2
-
-            IF (F_SQMOD(N/2-l1+1) > ent) THEN 
-                    ent = F_SQMOD(N/2-l1+1)
-                    t_ent = i*dt
-                    i_ent = i
-            ENDIF
-
-            IF (F_SQMOD(N/2+l1+1) > exi) THEN 
-                    exi = F_SQMOD(N/2+l1+1)
-                    t_exi = i*dt
-                    i_exi = i
-            ENDIF
-
-            IF (MOD(i, save_timestep) == 0) F_EVOL(:, i / save_timestep + 1) = F_SQMOD(:)
-
-            IF (cc == 0) THEN
-                    IF (F_SQMOD(N/2-l1+1) > 0.0001) THEN 
-                            time_hartman = i*dt
-                            cc = 1
-                            PRINT *, '1',i
-                    ENDIF
-            ENDIF
-                 
-            IF (cd == 0) THEN
-                    IF (F_SQMOD(N/2+l1-1) > 0.0001) THEN
-                            time_hartman = i*dt - time_hartman
-                            cd = 1
-                            PRINT *, '2', i
-                    ENDIF
-            ENDIF
- 
-        END DO
-        !$OMP END DO
-        !$OMP END PARALLEL
+        PRINT *, 'Computing wavepacket time evolution...'
         
-        l_1 = l_1 + 1.
-        x0 = x0 - 0.5
+        DO i = 1, M
+            PHI_X(:) = PHI_X(:) * V_OP(:)
+                        
+            PHI_K = FFT(PHI_X, 'F') 
+            PHI_K(:) = PHI_K(:) * K_OP(:) / N
 
-        T_B(j,1) = l_1
-        T_B(j,2) = time_hartman
-        T_B(j,3) = t_ent
-        T_B(j,4) = t_exi
+            PHI_X = FFT(PHI_K, 'B')
+            PHI_X(:) = PHI_X(:) * V_OP(:)
 
+            PHI_SQMOD(:) = REAL(PHI_X(:))**2 + AIMAG(PHI_X(:))**2
+
+            IF (MOD(i, save_timestep) == 0) PHI_EVOL(:, i / save_timestep + 1) = PHI_SQMOD(:) 
         END DO
-
-        OPEN(UNIT=10, FILE='hartman.txt')
-                WRITE(10, '(4F15.7)') (T_B(i,:), i=1,15)
-        CLOSE(10)
-
-        PRINT *, 'FINAL NORM:  ', SUM(F_EVOL(:,save_wave +1)) * (x(2)-x(1))
+        
+        PRINT *, 'computation done.'
         PRINT *, ''
-        PRINT *, 'TIME DELAY:', t_exi - t_ent
-        PRINT *, 'ENTERED AT', i_ent, ', EXITED AT', i_exi
 
         CALL WRITE_INTO_FILE()
+
+        PRINT *, 'The program has successfully completed the computation.'
+        PRINT *, 'Good bye.'
+        PRINT *, ''
+        PRINT *, ''
         
-        PRINT '(A, F7.5, A)', 'R  = ', SUM(F_EVOL(           : N/2 - l1   , save_wave+1)) * (x(2)-x(1)), ' %'
-        PRINT '(A, F7.5, A)', 'A1 = ', SUM(F_EVOL(N/2 - l1   : N/2 + l1   , save_wave+1)) * (x(2)-x(1)), ' %'
-        PRINT '(A, F7.5, A)', 'A12= ', SUM(F_EVOL(N/2 + l1   : 2*N/3 - l2 , save_wave+1)) * (x(2)-x(1)), ' %'
-        PRINT '(A, F7.5, A)', 'A2 = ', SUM(F_EVOL(2*N/3 - l2 : 2*N/3 + l2 , save_wave+1)) * (x(2)-x(1)), ' %'
-        PRINT '(A, F7.5, A)', 'T  = ', SUM(F_EVOL(2*N/3 + l2 :            , save_wave+1)) * (x(2)-x(1)), ' %'
-
-        peak = 0.D0
-        x_peak = 0.D0
-
-        DO i = 1, N
-          IF (i >= N/2 + l2) THEN
-                  IF (F_EVOL(i, save_wave+1) > peak) THEN
-                          peak = F_EVOL(i, save_wave+1)
-                          x_peak = x(i)
-                  END IF
-         END IF
-        END DO
-
-        PRINT *, 'Found peak at', x_peak
-
-        PRINT *, 'Hartman =', time_hartman
-
-
-          
         CONTAINS 
+                SUBROUTINE TITLE()
+                        PRINT *, ''
+                        PRINT *, 'NUMERICAL SOLUTION OF TIME DEPENDENT SCHROEDINGER EQUATION'
+                        PRINT *, '  QUANTUM WAVEPACKET TIME EVOLUTION'
+                        PRINT *, ''
+                        PRINT *, 'Code written by: Cuoghi A., Quinzi M., Rizzi G.'
+                        PRINT *, ''
+                        PRINT *, 'Dipartimento di Scienze Fisiche, Informatiche e Matematiche,'
+                        PRINT *, 'Università di Modena e Reggio Emilia'
+                        PRINT *, ''
+                        PRINT *, 'May 2021'
+                        PRINT *, ''
+                        PRINT *, ''
+                END SUBROUTINE TITLE
+                
                 SUBROUTINE WRITE_INTO_FILE()
-                        CHARACTER(20) :: FMT
+                        CHARACTER(50) :: FMT
+                        CHARACTER(14) :: f_pot  = 'Output/pot.txt'
+                        CHARACTER(17) :: f_evol = 'Output/moving.txt'
+                        CHARACTER(21) :: f_last = 'Output/final_wave.txt'
+                        CHARACTER(16) :: f_coef = 'Output/coeff.txt'
 
+                        PRINT *, 'Writing potential energy in "', f_pot, '"...'
+                        OPEN(UNIT=10, FILE=f_pot)
+                          WRITE(10, '(2F20.10)') (x(i), V(i), i = 1, N)
+                        CLOSE(10)
+                        PRINT *, ''
+
+                        PRINT *, 'Saving wavepacket evolution in "', f_evol, '"...'
                         WRITE(FMT, '("(", I0, "F20.10)")') save_wave + 1
-                        OPEN(UNIT = 10, FILE = 'Output/moving.txt')
-                          WRITE(10, FMT) (F_evol(i,:), i=1,N)
+                        OPEN(UNIT=10, FILE=f_evol)
+                          WRITE(10, FMT) (PHI_EVOL(i,:), i=1,N)
                         CLOSE(10)
+                        PRINT *, ''
 
-                        OPEN(UNIT = 10, FILE = 'Output/final_wave.txt')
-                          WRITE(10, '(2F20.10)') (F_X(i), i = 1, N)
+                        PRINT *, 'Saving last computed wavefunction in "', f_last, '"...'
+                        OPEN(UNIT=10, FILE=f_last)
+                          WRITE(10, '(2F20.10)') (PHI_X(i), i = 1, N)
                         CLOSE(10)
+                        PRINT *, ''
+
+                        PRINT *, 'Saving evolution parameters and computed coefficients in "', f_coef, '"...'
+                        OPEN(UNIT=10, FILE=f_coef, POSITION='APPEND')
+                         WRITE(10, '("L=", F20.10)') L
+                         WRITE(10, '("E1=", F20.10, ", l1=", F20.10, ", E2=", F20.10, ", l2=", F20.10)') E_1, l_1, E_2, l_2
+                         WRITE(10, '("s=", F20.10, ", x0=", F20.10, ", k0=", F20.10)') sigma, x0, k0
+                         WRITE(10, '("M=", I10, ", dt=", F20.10)') M, dt
+                         WRITE(10, *) 
+                         WRITE(10, '(A, F15.10, A)') 'R  = ', SUM(PHI_EVOL(: N/2-l1, save_wave+1)) * (x(2)-x(1)) * 100.D0, ' %'
+                         WRITE(10, '(A, F15.10, A)') 'A1 = ', SUM(PHI_EVOL(N/2-l1 : N/2+l1, save_wave+1)) * (x(2)-x(1)) * 100.D0, ' %'
+                         WRITE(10, '(A, F15.10, A)') 'A12= ', SUM(PHI_EVOL(N/2+l1 : 2*N/3-l2, save_wave+1)) * (x(2)-x(1)) * 100.D0, ' %'
+                         WRITE(10, '(A, F15.10, A)') 'A2 = ', SUM(PHI_EVOL(2*N/3-l2 : 2*N/3+l2, save_wave+1)) * (x(2)-x(1)) * 100.D0, ' %'
+                         WRITE(10, '(A, F15.10, A)') 'T  = ', SUM(PHI_EVOL(2*N/3+l2 : , save_wave+1)) * (x(2)-x(1)) * 100.D0, ' %'
+                         WRITE(10, *)
+                         WRITE(10, '(A, F12.10, A)') 'Initial norm = ', SUM(PHI_EVOL(:, 1)) * (x(2) - x(1))
+                         WRITE(10, '(A, F12.10, A)') 'Final norm = ', SUM(PHI_EVOL(:, save_wave+1)) * (x(2) - x(1))
+                         WRITE(10, *)
+                         WRITE(10, '(A)') '####################################'
+                         WRITE(10, *)
+                       CLOSE(10)
+                       PRINT *, ''
                 END SUBROUTINE WRITE_INTO_FILE
-
 END PROGRAM MAIN
 
